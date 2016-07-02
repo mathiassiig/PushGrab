30b1abe172c04dd8db8ff836a95f50055d0cc87d
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2016-07-02 14:05:42 +0200

    Improving visuals

---
+++ Platform Fighter/Assets/Scripts/Character/CharacterVisuals.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterVisuals : MonoBehaviour
+{
+    float BlinkDelay;
+    public Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBlink();
+    }
+
+    void UpdateBlink()
+    {
+        BlinkDelay -= Time.deltaTime;
+        if (BlinkDelay <= 0)
+        {
+            animator.SetTrigger("BlinkNow");
+            BlinkDelay = Random.Range(GlobalDefinitions.ANIM_CHARACTER_BLINK_MIN_DELAY, GlobalDefinitions.ANIM_CHARACTER_BLINK_MAX_DELAY);
+        }
+    }
+
+    void VisualDeath(GlobalDefinitions.DMGTYPE type, Vector2 direction)
+    {
+        switch (type)
+        {
+            case GlobalDefinitions.DMGTYPE.BLUNT:
+                break;
+            case GlobalDefinitions.DMGTYPE.EXPLOSION:
+                break;
+            case GlobalDefinitions.DMGTYPE.FIRE:
+                break;
+            case GlobalDefinitions.DMGTYPE.PIERCING:
+                break;
+        }
+    }
+}
---
+++ Platform Fighter/Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    public float MAXHP;
+    public float HP;
+    public bool Healable;
+    public bool Invincible;
+
+
+    public virtual void Damage(float amount)
+    {
+        HP -= amount;
+    }
+
+    public virtual void Damage(float amount, bool stuns, GlobalDefinitions.DMGTYPE type, Vector2 direction) //add direction
+    {
+        Damage(amount);
+    }
+
+    public virtual void Death()
+    {
+
+    }
+
+    public virtual void Death(GlobalDefinitions.DMGTYPE type, Vector2 direction)
+    {
+
+    }
+}
